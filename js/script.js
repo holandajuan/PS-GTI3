311cf9a9f9815f1c328ad28b4da3feb269ee5253
holandajuan <juanholandagithub@gmail.com>
2023-11-21 19:29:03 -0300

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,66 @@
+const listaPokemons = document.querySelector('.lista-pokemons ul');
+const nomeTime = document.getElementById('nome-time');
+let selectedPokemons = [];
+
+async function fetchPokemons() {
+    try {
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
+        const data = await response.json();
+        const pokemons = data.results;
+        mostraPokemons(pokemons);
+    } catch (error) {
+        console.error('Erro durante a requisição', error);
+    }
+}
+
+function mostraPokemons(pokemons) {
+    pokemons.forEach(async (pokemon) => {
+        const response = await fetch(pokemon.url);
+        const data = await response.json();
+        
+        const card = criaCardPokemon(data);
+
+        card.addEventListener("click", function () {
+            handleCardClick(card);
+        });
+        listaPokemons.appendChild(card);
+    });
+}
+
+function criaCardPokemon(pokemon) {
+    const card = document.createElement('li');
+    card.classList.add('card');
+    card.style.listStyle = 'none';
+
+    const img = document.createElement('img');
+    img.src = pokemon.sprites.front_default;
+    img.alt = pokemon.name;
+
+    
+    card.appendChild(img);
+
+    const name = document.createElement('p');
+    name.textContent = `Nome: ${pokemon.name}`;
+    const id = document.createElement('p');
+    id.textContent = `ID: ${pokemon.id}`;
+
+    const types = document.createElement('p');
+    types.textContent = `Tipos: ${pokemon.types.map(tipo => tipo.type.name).join(', ')}`;
+
+    card.appendChild(name);
+    card.appendChild(id);
+    card.appendChild(types);
+
+    return card;
+}
+
+let arrayTime = [];
+function handleCardClick(pokemon) {
+    if (arrayTime.length >= 6) return;
+
+    const time = document.querySelector('#formação-time ul');
+    time.appendChild(pokemon);
+    arrayTime.push(pokemon);
+}
+
+fetchPokemons();
